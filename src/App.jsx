445bfe9cbcaad9f9ac92f22246cd10677f142818
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2024-02-26 02:35:04 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './shared/Navbar/Navbar'
+function App() {
+  
+
+  return (
+    <div> 
+      <Navbar/>
+  
+
+      
+    </div>
+  )
+}
+
+export default App
